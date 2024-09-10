05b8700b8fab9d8e583404bb3db054a9994f0a62
MuratYucedag <yucedagmurat23@gmail.com>
2024-09-10 15:39:53 +0300

    Add project files.

---
+++ Project1Portfolio/Controllers/EducationController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project1Portfolio.Controllers
+{
+    public class EducationController : Controller
+    {
+        // GET: Education
+        public ActionResult EducationList()
+        {
+            return View();
+        }
+
+        public ActionResult CreateEducation()
+        {
+            return View();
+        }
+    }
+}
