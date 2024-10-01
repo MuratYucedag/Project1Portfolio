48a53514533f2286cf133ec190408ced2320e598
MuratYucedag <yucedagmurat23@gmail.com>
2024-10-01 16:05:51 +0300

    ...

---
+++ Project1Portfolio/Controllers/DefaultController.cs
@@ -0,0 +1,58 @@
+﻿using Project1Portfolio.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project1Portfolio.Controllers
+{
+    public class DefaultController : Controller
+    {
+        MyPortfolio5DbEntities context = new MyPortfolio5DbEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public PartialViewResult PartialHead()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialNavbar()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialHeader()
+        {
+            ViewBag.title = context.About.Select(x => x.Title).FirstOrDefault();
+            ViewBag.detail = context.About.Select(x => x.Detail).FirstOrDefault();
+            ViewBag.imageUrl = context.About.Select(x => x.ImageUrl).FirstOrDefault();
+
+            ViewBag.address = context.Profile.Select(x => x.Address).FirstOrDefault();
+            ViewBag.email = context.Profile.Select(x => x.Email).FirstOrDefault();
+            ViewBag.phone = context.Profile.Select(x => x.PhoneNumber).FirstOrDefault();
+            ViewBag.github = context.Profile.Select(x => x.Github).FirstOrDefault();
+
+            return PartialView();
+        }
+        public PartialViewResult PartialAbout()
+        {
+            ViewBag.title = context.Profile.Select(x => x.Title).FirstOrDefault();
+            ViewBag.description = context.Profile.Select(x => x.Description).FirstOrDefault();
+            ViewBag.phone = context.Profile.Select(x => x.PhoneNumber).FirstOrDefault();
+            ViewBag.email = context.Profile.Select(x => x.Email).FirstOrDefault();
+            ViewBag.imageUrl = context.Profile.Select(x => x.ImageUrl).FirstOrDefault();
+
+            return PartialView();
+        }
+        public PartialViewResult PartialEducation()
+        {
+            var values = context.Education.ToList();
+            return PartialView(values);
+        }
+        public PartialViewResult PartialScript()
+        {
+            return PartialView();
+        }
+    }
+}
